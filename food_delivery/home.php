099541123853646e6b8016edfd1f2c5686be2b2f
Shubham Ranjan <shubhamranjan542@gmail.com>
2018-07-04 15:05:04 +0530

    home_page

---
+++ food_delivery/home.php
@@ -0,0 +1,92 @@
+<html>
+	<head>
+		<title>Grab Swipe Serve</title>
+		<link rel="stylesheet" href="css/style.css">
+	</head>
+	<body>
+		<div class="top">
+			<div class="first">
+			</div>
+			<div class="second">
+				<select name="Location" Select Your Area>
+					<option value="" disabled selected>Select Your Area</option>
+  					<option value="Parnasree">Parnasree</option>
+					<option value="New Town">New Town</option>
+					<option value="Park Street">Park Street</option>
+					<option value="Kalikapur">Kalikapur</option>
+				</select>
+			</div>
+			<div class="third">
+				<div class="search">
+					<input type="text" placeholder="Search for restaurants..">
+				</div>
+				<div class="signin">
+					<button class="sign">Sign In</button>
+				</div>
+				<div class="signup">
+					<button class="create">Create an Account</button>
+				</div>
+			</div>			
+		</div>
+		<div class="bottom">
+			<div class="center">
+				<div class="zero_1">
+					<p>5 Restaurants....</p>
+				</div>
+				<div class="first_1">
+					<div class="menu">
+						<div class="menu1">
+							<div class="food1">
+									<img class="img1" src="img/kaleidoscope.jpeg"/>
+							</div>
+							<div class="name1">
+								<p>Kaleidoscope Multicuisine</p>
+							</div>
+						</div>
+						<div class="menu2">
+							<div class="food2">
+								<img class="img2" src="img/rangde.jpeg"/>
+							</div>
+							<div class="name2">
+								<p>Rang De Basanti Dhaba</p>
+							</div>						
+						</div>
+					</div>
+					<div class="switch">
+						<div class="Previous">
+							<button class="prev prev1">Previous</button>
+						</div>
+						<div class="Next">
+							<button class="nex nex1">Next</button>
+						</div>
+					</div>
+				</div>
+				<div class="second_1">
+				</div>
+				<div class="third_1">
+					<div class="quotes">
+						<div class="first_2">
+							<p class="show1">Unexpected Guests??</p>
+						</div>
+						<div class="second_2">
+							<p>Hot And Tasty Food is Waiting for U</br><span>Order Now!!!</span></p>
+						</div>
+					</div>
+					<div class="awards">
+					</div>
+				</div>
+				<div class="fourth_1">
+					<div class="name">
+						<h2>Grab Swipe Serve</h2>
+					</div>
+					<div class="logo">
+						<img class="fb" src="img/fb.jpeg"/>
+						<img class="tweet" src="img/twitter.jpeg"/>
+					</div>
+				</div>
+			</div>
+		</div>
+		<script type="text/javascript" src="js/script.js"></script>
+	</body>
+</html>
+		
